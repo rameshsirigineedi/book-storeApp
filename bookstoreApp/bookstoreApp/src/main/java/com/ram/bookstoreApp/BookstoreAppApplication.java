0fe7e0193ce5b706958efa3408533a626e4cc026
sirig <sirig@RaMeshS>
2020-04-03 16:40:37 +0530

    initial commit. created spring boot project and configure the application properties.

---
+++ bookstoreApp/bookstoreApp/src/main/java/com/ram/bookstoreApp/BookstoreAppApplication.java
@@ -0,0 +1,13 @@
+package com.ram.bookstoreApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreAppApplication.class, args);
+	}
+
+}
